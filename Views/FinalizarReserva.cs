1066f09f402f77148789243ed996a2f912d5ca5c
f-porto <felipe.portosa@gmail.com>
2024-04-25 18:26:08 -0300

    add can end reservas

--- Views/FinalizarReserva.cs
+++ Views/FinalizarReserva.cs
@@ -1,3 +1,4 @@
+using DesafioProjetoHospedagem.Models;
 using Terminal.Gui;
 
 namespace DesafioProjetoHospedagem.Views;
@@ -7,5 +8,111 @@ public class FinalizarReserva : HotelView
     public override void Init()
     {
         Title = "Finalizar Reserva";
+
+        var (abaixo, direita) = this.AdicionarLabels("ID da reserva", "ID da suíte", "Dias Reservados", "Valor da diária", "Valor final");
+        var entradaIdReserva = new TextField()
+        {
+            Y = 1,
+            X = direita + 1,
+            Width = Dim.Fill(),
+        };
+        var entradaIdSuite = new TextField()
+        {
+            X = direita + 1,
+            Y = 3,
+            Width = Dim.Fill(),
+        };
+        var labelDiasReservados = new Label()
+        {
+            X = direita + 1,
+            Y = 5,
+        };
+        var labelValorDiaria = new Label()
+        {
+            X = direita + 1,
+            Y = 7,
+        };
+        var labelValorFinal = new Label()
+        {
+            X = direita + 1,
+            Y = 9,
+        };
+
+        var botaoProcurar = new Button("Procurar")
+        {
+            X = direita + 1,
+            Y = abaixo,
+        };
+        var botaoFinalizar = new Button("Finalizar")
+        {
+            X = Pos.Right(botaoProcurar) + 1,
+            Y = abaixo,
+        };
+
+        Reserva reserva = null;
+        botaoProcurar.Clicked += () =>
+        {
+            var idReservaTexto = entradaIdReserva.Text.ToString().Trim();
+            if (idReservaTexto.Length != 0)
+            {
+                if (!int.TryParse(idReservaTexto, out int idReserva))
+                {
+                    this.CriarDialogo("Erro", $"'{idReservaTexto}' não é um id válido de reserva");
+                    return;
+                }
+                reserva = _db.GetReservaById(idReserva);
+                if (reserva is null)
+                {
+                    this.CriarDialogo("Erro", $"Reserva com id '{idReservaTexto}' não existe");
+                    return;
+                }
+                entradaIdSuite.Text = reserva.Suite.Id;
+                labelDiasReservados.Text = reserva.DiasReservados.ToString();
+                labelValorDiaria.Text = reserva.Suite.ValorDiaria.ToString();
+                labelValorFinal.Text = CalcularValorFinal(reserva.DiasReservados, reserva.Suite.ValorDiaria).ToString();
+                return;
+            }
+            var idSuite = entradaIdSuite.Text.ToString().Trim();
+            var suite = _db.GetSuiteById(idSuite);
+            if (suite is null)
+            {
+                this.CriarDialogo("Erro", $"Não existe suíte com id '{idSuite}'");
+                return;
+            }
+            if (!suite.Reservado)
+            {
+                this.CriarDialogo("Erro", $"A suíte '{idSuite}' não for reservada");
+                return;
+            }
+            reserva = _db.GetReservaById(suite.IdReserva);
+            entradaIdReserva.Text = reserva.Id.ToString();
+            labelDiasReservados.Text = reserva.DiasReservados.ToString();
+            labelValorDiaria.Text = reserva.Suite.ValorDiaria.ToString();
+            labelValorFinal.Text = CalcularValorFinal(reserva.DiasReservados, reserva.Suite.ValorDiaria).ToString();
+        };
+        botaoFinalizar.Clicked += () =>
+        {
+            if (reserva is null)
+            {
+                this.CriarDialogo("Erro", "Reserva não foi selecionada");
+                return;
+            }
+            reserva.Suite.Reservado = false;
+            _db.DeleteReserva(reserva.Id);
+            _db.UpdateSuite(reserva.Suite);
+            this.CriarDialogo("Sucesso", $"Reserva '{reserva.Id}' for finalizada");
+        };
+
+        Add(entradaIdReserva, entradaIdSuite, labelDiasReservados, labelValorDiaria, labelValorFinal, botaoProcurar, botaoFinalizar);
+    }
+
+    private static decimal CalcularValorFinal(int diasReservados, decimal valorDiaria)
+    {
+        var valorFinal = diasReservados * valorDiaria;
+        if (diasReservados >= 10)
+        {
+            valorFinal *= 0.9m;
+        }
+        return valorFinal;
     }
 }
